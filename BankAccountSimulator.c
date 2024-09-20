493c1329f60052c2cc52cc71d41d1b7c172fadc5
Ardi Grapci <ardiehnberggrapci@gmail.com>
2024-09-20 11:39:01 +0200

    Added creating an account and depositing money.

--- BankAccountSimulator.c
+++ BankAccountSimulator.c
@@ -3,8 +3,33 @@
 #include <string.h>
 
 // Define the Account structure
-struct Account {
+struct Account 
+{
     int accountNumber;
     char name[100];
     float balance;
 };
+
+// Creating an Account
+struct Account creatAccount(int accountNumber, char name[], float initialDeposit)
+{
+    struct Account newAccount;
+    newAccount.accountNumber = accountNumber;
+    strcpy(newAccount.name, name);
+    newAccount.balance = initialDeposit;
+    return newAccount;
+}
+
+// Depositing Money
+void deposit(struct Account *account, float amount)
+{
+    if (amount > 0)
+    {
+        account->balance += amount;
+        printf("Successfully deposited $%.2f. New balance $%.2f\n", amount, account->balance);
+    }
+    else 
+    {
+        printf("Invalid deposit amount.\n");
+    }
+}
